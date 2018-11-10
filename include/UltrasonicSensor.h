c67003ff4b7b9523ff311d6c538a85d4ed045fa4
NicoNelli <nicolobastianelli@gmail.com>
2018-11-10 11:22:41 +0100

    updated .h file

--- include/UltrasonicSensor.h
+++ include/UltrasonicSensor.h
@@ -4,7 +4,10 @@
 #include "lcm_messages/geometry/UltrasonicPosition.hpp"
 #include "lcm/lcm-cpp.hpp"
 #include <queue>
+
 #include "utils/TimeHelpers.hpp"
+
+
 class UltrasonicSensor {
 
 public:
@@ -13,22 +16,22 @@ public:
 	
 	double distance( int timeout ); //It computes the distance
 
-	bool GetValidity() const; //It checks if the result is valid(based on the timeout expired )	
+	bool GetValidity() const; //It checks if the result is valid(based on the expired timeout )	
 
 private:
 
 	bool isExpired( int timeout, long time ); //it checks if the timeout is expired. 	
 
 	void TimeOfTravel(); 
-	//It computes the the amount of time it takes for the pulse to go and come back.
+	//It computes the amount of time it takes for the pulse to go and come back.
 
 	int trigger; //trigger pin
 
 	int echo; //echo pin
 
-	volatile long startTimeUsec;
+	volatile long startTimeUsec; //start timing
 
-	volatile long endTimeUsec;
+	volatile long endTimeUsec; //end timing
 
 	double distanceMeters;
 
@@ -39,18 +42,58 @@ private:
 	bool isValid; //timeout is expired? if yes, the data are not valid.
 };
 
-/*It allows to estimate the velocity based on two measurements.
+
+
+/*It allows to estimate the velocity based on two measurements (difference quotient).
 Parameters:
 1) z position of the platform
 2) previous z position of the platform
 3) FIFO queue.
-4) flag to control the first time the function is used.
-5) time object to obtain dt for computing the temporal derivative. 
+4) flag to control the first time the function is called.
+5) time object to obtain dt for computing the difference quotient. 
 */
-void EstimatedVelocity(geometry::UltrasonicPosition *Actual_plat, geometry::UltrasonicPosition *Prev_plat, std::queue<double> *queue_t, bool *flag, const TimeManager& time, double *int_z);
+void EstimatedVelocity(geometry::UltrasonicPosition *Actual_plat, geometry::UltrasonicPosition *Prev_plat, std::queue<double> *queue_t, bool *flag, TimeManager& time, double *int_z);
+
 
 //mean value.
 void mean(double& sum,double plus,double minus);
+
+
+/*
+Kalman filter with one state variable which is the distance obtained by the sensor.
+
+Pseudo-code:
+
+Pred_Xk = F * Pred_Xk_1
+
+Pred_Pk = F * Pred_P_k_1 * F^t + Q
+
+Kk = Pred_Pk * H^t(H * Pred_Pk * H^t + R)^(-1)
+
+Xk = Pred_Xk + Kk * (Zk - H * Pred_Xk)
+
+Pk = (I - Kk * H) * Pred_Pk 
+
+--state: state variable
+
+--Pred_P_k_1: prediction of covariance
+
+--Zk: measurement
+
+--Xk: update the estimate of the state
+
+--Pk: update the covariance error
+
+--Q: covariance of process noise
+
+--R: covariance of the measurement noise
+*/
+void KF(double state, double Pred_P_k_1, double  Zk, double *Xk, double *Pk, double Q, double R);
+
+double movingAvarageFilter(double distance, std::queue<double> *buffer, double *currentValue, int window);
+
+
+
 #endif
  
 
